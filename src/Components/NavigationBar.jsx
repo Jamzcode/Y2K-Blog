398708e8176305d4f8a5c0cbf3d67284a3cf02e3
Jamzcode <129902520+Jamzcode@users.noreply.github.com>
2026-08-20 13:39:24 -0500

    Style: stylized blog preview of home page

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import {author, useAuth} from "../context/AuthContext"
 
 export default function NavigationBar() {
   const [showNav, setShowNav] = useState(false);
--- src/Components/PreviewCard.jsx
+++ src/Components/PreviewCard.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { getPublishedPosts } from "../api/posts";
+import pic1 from "../assets/AB_headshot.jpeg";
 
 export default function PreviewCard() {
   const [publishedBlogs, setPublishedBlogs] = useState([]);
@@ -20,9 +21,19 @@ export default function PreviewCard() {
     <div className="flex justify-center gap-2">
       {publishedBlogs.slice(0, 2).map((blog, key) => (
         <div>
-          <div className="bg-slate-50 rounded-lg border-4 border-slate-400 p-2 w-xl h-50 ">
-            <div className="text-amber-600 italic">01-25-2026</div>
-            <img src=""/>
+          <div className="bg-slate-50 rounded-lg border-3 border-slate-400 p-2 w-xl h-50 ">
+            <div className="text-amber-600 font-semibold italic">
+              01-25-2026
+            </div>{" "}
+            <div className="flex gap-2 bg-amber-200 rounded-l-3xl">
+              <img
+                src={pic1}
+                className="w-10 h-10 rounded-3xl border-2 border-slate-500"
+              />
+              <div className="flex items-center">
+                <p>Dr. B</p>
+              </div>
+            </div>
             <h1 className="font-black">{blog.title}</h1>{" "}
             <p className="text-xs text-slate-600 font-semibold ">
               {blog.content}
